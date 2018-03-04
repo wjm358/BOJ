610afe252483e21b4e70720ecdc6915339a1b7fa
wjm358 <wjm358@nate.com>
2018-03-05 00:43:08 +0900

    그대로 출력하기

---
+++ Baekjoon/src/BOJ11718/Main.java
@@ -0,0 +1,22 @@
+package BOJ11718;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String str;
+
+		while ((str = reader.readLine()) != null)
+			System.out.println(str);
+
+	}
+
+} {
+
+}
