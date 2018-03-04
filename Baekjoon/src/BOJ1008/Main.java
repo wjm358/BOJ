e3fe6f856df246b6bea48aa8a592cd80591b6d39
wjm358 <wjm358@nate.com>
2018-03-05 01:55:25 +0900

    A/B

---
+++ Baekjoon/src/BOJ1008/Main.java
@@ -0,0 +1,14 @@
+package BOJ1008;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		double a = keyboard.nextInt();
+		double b = keyboard.nextInt();
+		double c = a / b;
+
+		System.out.println(c);
+	}
+}
