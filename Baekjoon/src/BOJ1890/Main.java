29e53b6c82cf2db3dc0aeffcb2ff8862acebaa76
wjm358 <wjm358@nate.com>
2018-03-24 19:33:27 +0900

    점프

--- Baekjoon/src/BOJ1890/Main.java
+++ Baekjoon/src/BOJ1890/Main.java
@@ -5,6 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/*
+ *  정답이 2^63-1 보다 작거나 같으므로 단순한 bfs, dfs로 해결할 수 없음
+ *  -> dynamic programming 이용 
+ *  경로 개수 계산 과정에서 int값의 범위를 넘어갈 수 있으므로 long 타입으로 선언
+ */
 public class Main {
 	static int n;
 
@@ -28,14 +33,15 @@ public class Main {
 			for (int j = 0; j < n; j++) {
 				if (dp[i][j] == 0 || (i == n - 1 && j == n - 1))
 					continue;
+				
 				int jump = arr[i][j];
+				
 				if (inSide(i, j + jump))
 					dp[i][j + jump] += dp[i][j];
 				if (inSide(i + jump, j))
 					dp[i + jump][j] += dp[i][j];
 			}
 		}
-
 		System.out.println(dp[n - 1][n - 1]);
 	}
 
